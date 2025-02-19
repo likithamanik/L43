5c1f61c374c9338f18f088256721436d286469a4
likithamanik <likhitha.naik@s.amity.edu>
2025-02-19 14:39:51 +0530

    phone no,address

--- html.c
+++ html.c
@@ -12,4 +12,14 @@
 <td><strong>age</strong></td>
 <td>18</td>
 </tr>
-
+<tr>
+<td><strong>phone no</strong></td>
+<td>1234</td>
+</tr>
+<tr>
+<td><strong>address</strong></td>
+<td>banglore</td>
+</tr>
+</table>
+</body>
+</html>
