c5ba90b77161975c27c566767bc25c54556b7324
likithamanik <likhitha.naik@s.amity.edu>
2025-03-04 20:03:17 +0530

    Factorial

--- fac.c
+++ fac.c
@@ -6,4 +6,10 @@ int main() {
 	scanf("%d",&num);
    if(num<0){
 	   printf("Factorial is not defined for negative numbers.\n");
+   }else{
+	   for(int i=1;i<num;i++){
+		   factorial *=i;
+	   }
+	   printf("Factorial of %d is:%d\n",num,factorial);
+	   return 0;
    }
