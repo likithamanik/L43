58f7ccc968e02bd0f0c7e3074c3bc0f84d9affca
likithamanik <likhitha.naik@s.amity.edu>
2025-03-19 13:42:57 +0530

    temp

---
+++ 19march/temp.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main(){
+	float fahrenheit,celsius:
+		printf("enter temp in fahrenheit:");
+	scanf("%f",&fahrenheit")
+		celsius=(fahrenheit-32)*5/9);
+	printf("temperature in celsius:%.2f\n",celsius);
+	return 0;
+}#include <stdio.h>
+int main(){
+	float fahrenheit,celsius:
+		printf("enter temp in fahrenheit:");
+	scanf("%f",&fahrenheit")
+		celsius=(fahrenheit-32)*5/9);
+	printf("temperature in celsius:%.2f\n",celsius);
+	return 0;
+}#include <stdio.h>
+int main(){
+	float fahrenheit,celsius:
+		printf("enter temp in fahrenheit:");
+	scanf("%f",&fahrenheit")
+		celsius=(fahrenheit-32)*5/9);
+	printf("temperature in celsius:%.2f\n",celsius);
+	return 0;
+}#include <stdio.h>
+int main(){
+	float fahrenheit,celsius:
+		printf("enter temp in fahrenheit:");
+	scanf("%f",&fahrenheit")
+		celsius=(fahrenheit-32)*5/9);
+	printf("temperature in celsius:%.2f\n",celsius);
+	return 0;
+}#include <stdio.h>
+int main(){
+	float fahrenheit,celsius:
+		printf("enter temp in fahrenheit:");
+	scanf("%f",&fahrenheit")
+		celsius=(fahrenheit-32)*5/9);
+	printf("temperature in celsius:%.2f\n",celsius);
+	return 0;
+}
